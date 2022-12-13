1b151447c0d6afad7e5065c9b3e88e6d0156c1b4
edouard <edouard.blais@hotmail.com>
2022-12-13 16:46:02 -0500

    setup mongoDB

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+const mongoDB = "mongodb+srv://edouardblais:Sk84life!@cluster0.utbckoz.mongodb.net/inventory_app?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
