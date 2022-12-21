6a6d9ec60fff60eb774f36cbf3dd8cea6a473ad2
edouard <edouard.blais@hotmail.com>
2022-12-20 21:29:19 -0500

    fixed vulnerability

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const dev_db_url =
-  "mongodb+srv://edouardblais:sk84life@cluster0.utbckoz.mongodb.net/inventory_app?retryWrites=true&w=majority";
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
